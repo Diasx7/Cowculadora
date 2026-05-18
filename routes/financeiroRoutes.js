7681daae812d127d7ea240a4fbd032db74e95ab8
Joao Pablo Souza Dias <joaopablosouzadias79@gmail.com>
2026-05-18 15:35:07 -0300

    fix: corrige case medicamentoController C maiusculo

--- routes/financeiroRoutes.js
+++ routes/financeiroRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/financeirocontroller");
+const controller = require("../controllers/financeiroController");
 const verificarToken = require("../middleware/authMiddleware");
 
 router.get("/cotacao", verificarToken, controller.getCotacao);
--- routes/medicamentosRoutes.js
+++ routes/medicamentosRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/medicamentocontroller");
+const controller = require("../controllers/medicamentoController");
 const verificarToken = require("../middleware/authMiddleware");
 
 router.post("/", verificarToken, controller.registrar);
