12184739e56d323b5727259e8b495aabb8fbc597
Joao Pablo Souza Dias <joaopablosouzadias79@gmail.com>
2026-05-18 15:56:42 -0300

    fix: db usa variaveis de ambiente

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "cowculadora"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "1234",
+  database: process.env.DB_NAME || "cowculadora"
 });
 
 module.exports = db;
